a21044fb74a1c8159fc1b2c37a45d4739de0aed1
rafinhadufluxo <arruda.rafa.ra@gmail.com>
2024-07-21 13:46:53 -0400

    create menu para um dashboard \0/

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div :class="['app', is_expanded ? 'menu-expanded' : 'menu-collapsed']">
+    <!-- Sidebar -->
+    <AppSidebar @toggle-menu="toggleMenu" />
+
+    <!-- Content -->
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+import { ref } from "vue";
+import AppSidebar from "./components/Sidebar.vue";
+
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true");
+
+const toggleMenu = () => {
+  is_expanded.value = !is_expanded.value;
+  localStorage.setItem("is_expanded", is_expanded.value);
+};
+</script>
+
+<style lang="scss">
+:root {
+  --primary: #4ade80;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
 }
 
-nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  background: var(--light);
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  &.menu-expanded {
+    main {
+      display: none;
+    }
+  }
+
+  &.menu-collapsed {
+    main {
+      flex: 1 1 0;
+      padding: 2rem;
+
+      @media (max-width: 1024px) {
+        padding-left: 6rem;
+      }
+    }
+  }
 }
 </style>
